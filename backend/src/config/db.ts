234f1383be6fa873ede66436d66fcef3cf41575b
AlonShab <alonshabtay@gmail.com>
2025-02-07 01:21:06 +0200

    connected FE to BE

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -3,7 +3,6 @@ import mongoose from "mongoose";
 export const connectDb = async () => {
     try {
         await mongoose.connect(process.env.MONGODB_URI as string);
-        debugger;
         console.log('db connected!');
     } catch (error) {
         console.error("MongoDB connection error:", error);
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,8 +1,10 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
 
 module.exports = {
     mode: "development",
+    devtool: "source-map",
     entry: "./src/index.tsx",
     output: {
         path: path.resolve(__dirname, "dist"),
@@ -25,7 +27,8 @@ module.exports = {
         ]
     },
     plugins: [
-        new HtmlWebpackPlugin({ template: "./public/index.html" })
+        new HtmlWebpackPlugin({ template: "./public/index.html" }),
+        new Dotenv({})
     ],
     devServer: {
         static: path.join(__dirname, "dist"),
